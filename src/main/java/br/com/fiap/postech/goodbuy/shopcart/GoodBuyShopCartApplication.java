fa3e55422071e60e0f9c5a560713bc492f5ee50e
Anderson0Wagner <anderson.wagner@gmail.com>
2024-06-14 10:29:04 -0300

    criação do projeto com spring boot

---
+++ src/main/java/br/com/fiap/postech/goodbuy/shopcart/GoodBuyShopCartApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.postech.goodbuy.shopcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodBuyShopCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodBuyShopCartApplication.class, args);
+	}
+
+}
